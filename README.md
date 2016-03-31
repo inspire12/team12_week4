c5b2caba8b044f5e3525fdd54e996dfa72fc34b3
seo <ox4443@naver.com>
2016-03-31 15:36:30 +0900

    commit reset

---
+++ README.md
@@ -0,0 +1,2 @@
+# team12_week4
+bill program
---
+++ real/src/test/java/com/your/dream/real/AppTest.java
@@ -0,0 +1,13 @@
+package com.your.dream.real;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	@Test
+	public void testMainIsWorking() {
+		App.main(new String[] { "This is test!" });
+	}
+}
