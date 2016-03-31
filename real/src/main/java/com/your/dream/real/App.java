d0e5ef7c86a220aeb384c56e0f81de72146ed046
seo <ox4443@naver.com>
2016-03-31 16:01:28 +0900

    basic construction

--- real/src/main/java/com/your/dream/real/App.java
+++ real/src/main/java/com/your/dream/real/App.java
@@ -12,8 +12,14 @@ public class App
 	}
 	
     public static void main( String[] args ) {
+int total =0;
+int silver_rate;
+int gold_rate;
     	for(String arg : args) {
+		System.out.println("전화요금 관리 시스템을 시작합니다.");
     		System.out.println("++parameter: " + arg);
+		
+		System.out.println("총 전화요금은 "+ total +"입니다.");
     	}
     }
 }
---
+++ real/src/main/java/com/your/dream/real/Calculater.java
@@ -0,0 +1,17 @@
+package com.your.dream.real;
+
+/**
+ * Calcualtor
+ *
+ */
+public class App 
+{
+	
+	private App() 
+	{
+		// do not create App class directly
+	}
+	
+    	}
+    }
+}
