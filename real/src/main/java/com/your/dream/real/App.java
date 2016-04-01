1fa736fe8ee7868408d32391240b7a3b0523290e
inspire12 <ox4443@naver.com>
2016-04-01 23:36:41 +0900

    app upload
    convert app main

--- real/src/main/java/com/your/dream/real/App.java
+++ real/src/main/java/com/your/dream/real/App.java
@@ -1,4 +1,4 @@
-package com.your.dream.real;
+
 
 /**
  * Hello world!
@@ -12,14 +12,24 @@ public class App
 	}
 	
     public static void main( String[] args ) {
-int total =0;
-int silver_rate;
-int gold_rate;
-    	for(String arg : args) {
-		System.out.println("전화요금 관리 시스템을 시작합니다.");
-    		System.out.println("++parameter: " + arg);
-		
-		System.out.println("총 전화요금은 "+ total +"입니다.");
+    
+    	BasicPlan b;
+    	AppView _appview =new AppView();
+    	
+    	//while(){
+    	_appview.showProgramStartMSG();
+    	if(_appview.insertPlan()==1){//gold
+    		b=new Gold();
+    	}else{
+    		b=new Silver();
     	}
+    
+    	int number=_appview.insertUsingLine();
+    	float time=_appview.insertUsingTime();
+    	
+    	
+    	Calculator _calcuator = new Calculator(b, number, time);
+    	
+    	_appview.printResult(_calcuator.CalculatorBill());
     }
 }
